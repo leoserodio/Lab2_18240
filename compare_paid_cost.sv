e90256506af0d33af73f50730f03c7578480dabe
leoserodio <leoserodio06@gmail.com>
2026-02-07 16:39:39 -0500

    Update compare_paid_cost.sv

--- compare_paid_cost.sv
+++ compare_paid_cost.sv
@@ -6,13 +6,13 @@
     if paid = cost, exact goes high => 
     if paid > cost, more goes high.
 \******                             ******/
-module compare_paid_cost (
-  input  logic [3:0] paid,
-  input  logic [3:0] cost,
-  output logic more,
-  output logic exact,
-  output logic less
-);
+module compare_paid_cost 
+    (input  logic [3:0] paid,
+     input  logic [3:0] cost,
+     output logic more,
+     output logic exact,
+     output logic less
+    );
   always_comb begin
     less  = (paid < cost);
     exact = (paid == cost);
