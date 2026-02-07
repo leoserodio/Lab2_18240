3770e9694f7f88c4ad4f7cb571389210b46465e4
leoserodio <leoserodio06@gmail.com>
2026-02-07 16:41:28 -0500

    Update sub4.sv

--- sub4.sv
+++ sub4.sv
@@ -1,10 +1,9 @@
 `default_nettype none
 /******      4-bit Subtractor      ******/
-module sub4 (
-  input  logic [3:0] a,
-  input  logic [3:0] b,
-  output logic [3:0] diff
-);
+module sub4 
+  (input  logic [3:0] a,
+   input  logic [3:0] b,
+   output logic [3:0] diff);
   always_comb begin
     diff = a - b;
   end
